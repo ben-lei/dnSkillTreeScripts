b7821574885592b4a49a14d185d620a485f2afc2
Benjamin Lei <benlei.cse@gmail.com>
2016-11-11 03:03:25 -0800

    Optimized to work with with H2. Improved crest search (use item's job req + item's description).
    MySQL processing: 1m 20s
    H2 processing: 20s
    
    I think it's pretty obvious what will be used for gathering + publishing data :)
    
    Oh and added potential table to list of needed tables for techs.
    
    Oddly for getting weapons... h2 doesn't complain about message id being an int and the replaced string being a string.

--- sql/get-crests.prepared.sql
+++ sql/get-crests.prepared.sql
@@ -1,15 +1,11 @@
 SELECT
     g._SkillID,
-    g._SummaryDescription,
-    g._SummaryDescriptionParam
+    i._DescriptionID,
+    i._DescriptionIDParam
 FROM
     glyphskill g
         JOIN
     item i ON g.ID = i.ID
-        JOIN
-    skill s ON s.ID = g._SkillID
-WHERE
-    g._SkillID != 0 AND g._GlyphType = 2
-        AND i._DescriptionID != 1000098590
+WHERE i._DescriptionID != 1000098590
         AND i._Rank = 3
-        AND s._NeedJob IN (?, ?, ?)
+        AND i._NeedJobClass IN (? , ?, ?)
--- sql/get-weapons.sql
+++ sql/get-weapons.sql
@@ -1,8 +1,13 @@
-SELECT DISTINCT w._EquipType, m.ID as _NameID
-FROM item i
-JOIN weapon w
-	ON i.ID = w.ID
-JOIN message m
-	ON m.ID = REPLACE(REPLACE(i._NameIDParam, '{', ''), '}', '')
-WHERE i._NameID = 1000006853
-	AND i._LevelLimit = 1
+SELECT DISTINCT
+    w._EquipType, m.ID AS _NameID
+FROM
+    item i
+        JOIN
+    weapon w ON i.ID = w.ID
+        JOIN
+    message m ON m.ID = REPLACE(REPLACE(i._NameIDParam, '{', ''),
+        '}',
+        '')
+WHERE
+    i._NameID = 1000006853
+        AND i._LevelLimit = 1
