1c6979c3b55a855d0f6942bb461ce53e99afdbbd
Benjamin Lei <benlei.cse@gmail.com>
2017-02-08 23:09:40 -0800

    Removing useless properties from alt skills

--- sql/get-alt-skills.prepared.sql
+++ sql/get-alt-skills.prepared.sql
@@ -2,18 +2,11 @@ SELECT
     j._JobNumber,
     s.ID as _SkillID,
     s._NameID,
-    s._IconImageIndex,
-    s._NeedWeaponType1,
-    s._NeedWeaponType2,
-    s._MaxLevel, -- max skill level
-    s._SPMaxLevel,
     s._SkillType,
     s._DurationType, -- instant, buff, debuff, ex
     s._Element,
-    s._BaseSkillID, -- grouping of skill
     s._GlobalCoolTimePvP,
-    s._GlobalCoolTimePvE,
-    s._SkillGroup
+    s._GlobalCoolTimePvE
 FROM skill s
   JOIN job j
     ON j.ID = s._NeedJob
